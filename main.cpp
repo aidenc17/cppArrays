5d75907d2e1ffb08ac4228408878249ca70d6d38
aiden cox <cox.378@wright.edu>
2025-03-05 17:15:45 -0500

    basic arrays

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::string cars[] = {"C8 Corvette", "Mustang", "Camry", };
+
+    cars[1] = "Camero";
+
+    std::cout << cars[0] << '\n';
+    std::cout << cars[1] << '\n';
+    std::cout << cars[2] << '\n';
+
+
     return 0;
 }
