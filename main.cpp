6f27eb07d69c5975ef407d65a9815a42a032dc82
aiden cox <cox.378@wright.edu>
2025-03-05 23:51:39 -0500

    sizeof operator

--- main.cpp
+++ main.cpp
@@ -10,6 +10,20 @@ int main() {
     std::cout << cars[1] << '\n';
     std::cout << cars[2] << '\n';
 
+    //sideof operator
+
+    std::string name = "aiden";
+    double gpa = 2.5;
+    char grade = 'A';
+    bool student = true;
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    std::string students[] = {"John", "John", "John", "John"};
+
+    std::cout << sizeof(grade) << '\n';
+    std::cout << sizeof(gpa) << '\n';
+    std::cout << sizeof(name) << '\n';
+    std::cout << sizeof(student) << '\n';
+    std::cout << sizeof(grades) << '\n';
 
     return 0;
 }
